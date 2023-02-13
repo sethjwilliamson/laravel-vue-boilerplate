67ff86a0a582ca858bb63e637f3465a9c2cccfc9
Seth Williamson <sethjwilliamson@gmail.com>
2023-02-12 17:10:25 -0700

    Apply insights changes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -12,18 +12,14 @@ class Handler extends ExceptionHandler
      *
      * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
      */
-    protected $levels = [
-        //
-    ];
+    protected $levels = [];
 
     /**
      * A list of the exception types that are not reported.
      *
      * @var array<int, class-string<\Throwable>>
      */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport = [];
 
     /**
      * A list of the inputs that are never flashed to the session on validation exceptions.
@@ -43,8 +39,6 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        $this->reportable(function (Throwable $e) {});
     }
 }
